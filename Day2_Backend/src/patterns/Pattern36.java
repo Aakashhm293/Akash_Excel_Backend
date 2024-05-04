7fdc8cbfd854ab90933735a60234610eec711e7c
Akash HM <akashhm293@outlook.com>
2024-05-04 14:29:07 +0530

    Completed All the pattern programs

--- Day2_Backend/src/patterns/Pattern36.java
+++ Day2_Backend/src/patterns/Pattern36.java
@@ -3,40 +3,33 @@ package patterns;
 public class Pattern36 {
 	
 //	* * * * * * * 
-//	 * * * * * * 
 //	  * * * * * 
-//	   * * * * 
 //	    * * * 
-//	     * * 
 //	      * 
-//	     * * 
 //	    * * * 
-//	   * * * * 
 //	  * * * * * 
-//	 * * * * * * 
 //	* * * * * * * 
 	
 	public static void main(String[] args) {
-        int n = 7;
-        if (n == 7) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < i; j++) {
-                    System.out.print(" ");
-                }
-                for (int j = 0; j < n - i; j++) {
-                    System.out.print("* ");
-                }
-                System.out.println();
-            }
-            for (int i = 1; i < n; i++) {
-                for (int j = 0; j < 6 - i; j++) {
-                    System.out.print(" ");
-                }
-                for (int j = 0; j <= i; j++) {
-                    System.out.print("* ");
-                }
-                System.out.println();
-            }
-        }
-    }
+		int n = 7;
+		for (int i = 0; i < n / 2 + 1; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("  ");
+			}
+			for (int k = 0; k < n - 2 * i; k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+		for (int i = n / 2 - 1; i >= 0; i--) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("  ");
+			}
+			for (int k = 0; k < n - 2 * i; k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
 }
--- Day2_Backend/src/patterns/Pattern37.java
+++ Day2_Backend/src/patterns/Pattern37.java
@@ -1,5 +1,40 @@
 package patterns;
 
 public class Pattern37 {
-
+	
+//	1 2 3 4 3 2 1 
+//	  1 2 3 2 1 
+//	    1 2 1 
+//	      1 
+//	    1 2 1 
+//	  1 2 3 2 1 
+//	1 2 3 4 3 2 1 
+	
+	public static void main(String[] args) {
+		int n = 7;
+		for (int i = 1; i <= n / 2 + 1; i++) {
+			for (int j = 1; j < i; j++) {
+				System.out.print("  ");
+			}
+			for (int k = 1; k <= (n / 2 + 2) - i; k++) {
+				System.out.print(k + " ");
+			}
+			for (int k = (n / 2 + 1) - i; k >= 1; k--) {
+				System.out.print(k + " ");
+			}
+			System.out.println();
+		}
+		for (int i = n / 2; i >= 1; i--) {
+			for (int j = 1; j < i; j++) {
+				System.out.print("  ");
+			}
+			for (int k = 1; k <= (n / 2 + 2) - i; k++) {
+				System.out.print(k + " ");
+			}
+			for (int k = (n / 2 + 1) - i; k >= 1; k--) {
+				System.out.print(k + " ");
+			}
+			System.out.println();
+		}
+	}
 }
